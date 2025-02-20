cc91becb8500a519a8e1bb6c27c0ecdc5cce8bae
denkmoritz <moritzdenk0407@gmail.com>
2025-02-20 12:23:42 +0100

    change because of psycopg 3

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from app.config import Config
 def get_db_connection():
     connection = psycopg.connect(
         host=Config.DB_HOST,
-        database=Config.DB_NAME,
+        dbname=Config.DB_NAME,
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         port=Config.DB_PORT
