25f122b4bfa7725cad1e7739015b3b16372b0a3a
denkmoritz <moritzdenk0407@gmail.com>
2025-02-20 12:34:13 +0100

    small changes for config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_HOST = os.getenv("DB_HOST", "postgis")  # Use the Docker service name "postgis"
     DB_NAME = os.getenv("DB_NAME", "osm_data")
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
-    DB_PORT = os.getenv("DB_PORT", 9876)
+    DB_PORT = int(os.getenv("DB_PORT", "5432"))  # Inside Docker, the database uses port 5432
