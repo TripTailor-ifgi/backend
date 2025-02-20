ce28c7332f071b8fce4a7a47343d790d84e1a883
denkmoritz <moritzdenk0407@gmail.com>
2025-02-20 13:03:26 +0100

    change of host

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_HOST = os.getenv("DB_HOST", "postgis")
     DB_NAME = os.getenv("DB_NAME", "osm_data")
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
