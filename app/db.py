0c7864a5c4173a19295a0bd000b69e9e78f40c77
denkmoritz <moritzdenk0407@gmail.com>
2025-02-20 12:51:35 +0100

    change back to psy 2

--- app/db.py
+++ app/db.py
@@ -1,10 +1,10 @@
-import psycopg
+import psycopg2
 from app.config import Config
 
 def get_db_connection():
-    connection = psycopg.connect(
+    connection = psycopg2.connect(
         host=Config.DB_HOST,
-        dbname=Config.DB_NAME,
+        database=Config.DB_NAME,
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         port=Config.DB_PORT
